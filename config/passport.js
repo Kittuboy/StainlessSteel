452996f1b9dabb854e5503173257ea19160835dd
Kittu Boy <boykittu926@gmail.com>
2025-06-28 19:19:17 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -21,7 +21,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/github/callback"
+      callbackURL: process.env.GITHUB_CALL_BACK_URL
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
